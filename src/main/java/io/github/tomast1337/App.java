e9ce536673b3875893bc34c91dacd3771184b2bf
Tomast1337 <vycasnicolas@gmail.com>
2021-02-21 18:10:01 -0300

    Adicionado auto complete ao comando /sorter e refatorado algumas coisas

--- src/main/java/io/github/tomast1337/App.java
+++ src/main/java/io/github/tomast1337/App.java
@@ -1,23 +1,41 @@
 package io.github.tomast1337;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Sheep;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
-public class App extends JavaPlugin{
+public class App extends JavaPlugin {
     private static Sheep[] sheeplist;
+    public static final String[] sortCommandOptions = {"criar", "embaralhar", "destruir", "mover", "bubble", "insertion", "selection"};
+
     @Override
     public void onEnable() {
         sheeplist = new Sheep[16];
-        getLogger().info("Hello, SpigotMC! From The Greater Sorter");
-        Objects.requireNonNull(this.getCommand("Sorter")).setExecutor(new Sorter(sheeplist,this));
+        getLogger().info("Algoritmos de ordenação visualização");
+        Objects.requireNonNull(this.getCommand("Sorter")).setTabCompleter(new TabCompleter() {
+            @Override
+            public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+                return Arrays.asList(sortCommandOptions);
+            }
+        });
+        Objects.requireNonNull(this.getCommand("Sorter")).setExecutor(new Sorter(sheeplist, this));
     }
+
     @Override
     public void onDisable() {
         getLogger().info("See you again, SpigotMC!");
-        for(Sheep sheep :sheeplist)
-            sheep.damage(99);
+        try {
+            for (Sheep sheep : sheeplist)
+                sheep.damage(99);
+        }catch (NullPointerException e) {
+            getLogger().info("Nem uma ovelha destruida");
+        }
     }
 
 
