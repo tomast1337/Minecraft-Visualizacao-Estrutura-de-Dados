9fda29c9a88952aa6ce277fe341833d18d590d90
Tomast1337 <vycasnicolas@gmail.com>
2021-03-05 14:26:49 -0300

    Adicionado alguns algoritmos de ordenação e alterado o tipo da classe para enum

--- src/main/java/io/github/tomast1337/sorting/SortingAlg.java
+++ src/main/java/io/github/tomast1337/sorting/SortingAlg.java
@@ -2,52 +2,127 @@ package io.github.tomast1337.sorting;
 
 import org.bukkit.entity.Sheep;
 
-public class SortingAlg {
+public enum SortingAlg {
+    BUBBLE_SORT,
+    INSERTION_SORT,
+    SELECTION_SORT,
+    QUICK_SORT,
+    MERGE_SORT,
+    HEAP_SORT,
+    RADIX_SORT_BASE_2,
+    COCKTAIL_SORT,
+    NOT_SO_BOGO_SORT;
 
-    public static void bubbleSort(Sheep[] sheeplist) {
-        String passosAnimação = "";
-    }
+    public static String bubbleSort(Sheep[] sheeplist) {
+        StringBuilder passosAnimação = new StringBuilder("");
+        for (int i = 0; i < sheeplist.length; i++) {
+            for (int j = 1; j < (sheeplist.length - i); j++) {
+                if (Integer.parseInt(sheeplist[j - 1].getName()) > Integer.parseInt(sheeplist[j].getName())) {
+                    //TODO: adiciona animação
+                    Sheep aux = sheeplist[j - 1];
+                    sheeplist[j - 1] = sheeplist[j];
+                    sheeplist[j] = aux;
+                }
 
-    public static void insertionSort(Sheep[] sheeplist) {
-        String passosAnimação = "";
-    }
-
-    public static void selectionsort(Sheep[] sheeplist) {
-        String passosAnimação = "";
-    }
-
-    public static void quickSort(Sheep[] sheeplist) {
-        String passosAnimação = "";
-    }
-
-    public static void mergeSort(Sheep[] sheeplist) {
-        String passosAnimação = "";
-    }
-
-    public static void heapSort(Sheep[] sheeplist) {
-        String passosAnimação = "";
-    }
-
-    public static void Radix2Sort(Sheep[] sheeplist) {
-        String passosAnimação = "";
-    }
-
-    public static void cocktailSort(Sheep[] sheeplist) {
-        String passosAnimação = "";
-    }
-
-    public static void notSoBoggoSort(Sheep[] sheeplist) {
-        String passosAnimação = "";
-    }
-
-    public void swapByPos(Sheep[] sheeplist, int imdex1, int imdex2) {
-        try {
-            Sheep aux = sheeplist[imdex1];
-            sheeplist[imdex1] = sheeplist[imdex2];
-            sheeplist[imdex2] = aux;
-        } catch (Exception ignored) {
-
+            }
         }
+        return passosAnimação.toString();
     }
 
+    public static String insertionSort(Sheep[] sheeplist) {
+        StringBuilder passosAnimação = new StringBuilder("");
+        int n = sheeplist.length;
+        for (int i = 1; i < n; ++i) {
+            Sheep key = sheeplist[i];
+            int j = i - 1;
+            //TODO: adiciona animação
+            while (j >= 0 && Integer.parseInt(sheeplist[j].getName()) > Integer.parseInt(key.getName())) {
+                sheeplist[j + 1] = sheeplist[j];
+                j = j - 1;
+            }
+
+            sheeplist[j + 1] = key;
+        }
+        return passosAnimação.toString();
+    }
+
+    public static String selectionSort(Sheep[] sheeplist) {
+        StringBuilder passosAnimação = new StringBuilder("");
+
+        for (int i = 0; i < sheeplist.length - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < sheeplist.length; j++)
+                if (Integer.parseInt(sheeplist[j].getName()) < Integer.parseInt(sheeplist[min_idx].getName())) {
+                    min_idx = j;
+                }
+            //TODO: adiciona animação
+            Sheep aux = sheeplist[min_idx];
+            sheeplist[min_idx] = sheeplist[i];
+            sheeplist[i] = aux;
+        }
+
+        return passosAnimação.toString();
+    }
+
+    public static String cocktailSort(Sheep[] sheeplist) {
+        StringBuilder passosAnimação = new StringBuilder("");
+        boolean swapped = true;
+        int start = 0;
+        int end = sheeplist.length;
+        //TODO: adiciona animação
+        while (swapped) {
+            swapped = false;
+            for (int i = start; i < end - 1; ++i) {
+                if (Integer.parseInt(sheeplist[i].getName()) > Integer.parseInt(sheeplist[i + 1].getName())) {
+                    Sheep aux = sheeplist[i];
+                    sheeplist[i] = sheeplist[i + 1];
+                    sheeplist[i + 1] = aux;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+            swapped = false;
+            end = end - 1;
+            for (int i = end - 1; i >= start; i--) {
+                if (Integer.parseInt(sheeplist[i].getName()) > Integer.parseInt(sheeplist[i + 1].getName())) {
+                    Sheep aux = sheeplist[i];
+                    sheeplist[i] = sheeplist[i + 1];
+                    sheeplist[i + 1] = aux;
+                    swapped = true;
+                }
+            }
+            start = start + 1;
+        }
+        return passosAnimação.toString();
+    }
+
+    public static String notSoBoggoSort(Sheep[] sheeplist) {
+        StringBuilder passosAnimação = new StringBuilder("");
+
+        return passosAnimação.toString();
+    }
+
+    public static String mergeSort(Sheep[] sheeplist) {
+        StringBuilder passosAnimação = new StringBuilder("");
+
+        return passosAnimação.toString();
+    }
+
+    public static String quickSort(Sheep[] sheeplist) {
+        StringBuilder passosAnimação = new StringBuilder("");
+
+        return passosAnimação.toString();
+    }
+
+    public static String RadixSortBase2(Sheep[] sheeplist) {
+        StringBuilder passosAnimação = new StringBuilder("");
+
+        return passosAnimação.toString();
+    }
+
+    public static String heapSort(Sheep[] sheeplist) {
+        StringBuilder passosAnimação = new StringBuilder("");
+        return passosAnimação.toString();
+    }
 }
