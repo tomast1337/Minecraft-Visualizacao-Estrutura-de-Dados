d58a6b8f399372b2a92901ec56c2f9938f83e078
Tomast1337 <vycasnicolas@gmail.com>
2021-03-05 02:02:04 -0300

    Adicionado escalas estaticas

---
+++ src/main/java/io/github/tomast1337/util/Escalas.java
@@ -0,0 +1,7 @@
+package io.github.tomast1337.util;
+
+public class Escalas {
+    public static final float[] escalaMenorHarmobica = {0, 2, 3, 5, 7, 8, 11, 12, 14, 15, 17, 19, 20, 23, 24, 24};
+    public static final float[] escalaCromatica1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    public static final float[] escalaCromatica2 = {0, 1, 2, 4, 6, 8, 10, 12, 14, 14, 16, 18, 20, 22, 24, 24};
+}
